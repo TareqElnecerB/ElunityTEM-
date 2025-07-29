ef644c2c6c7ebfb27ebcf718ec0d6362087a360d
Alejo <alejandro221app@gmail.com>
2025-07-29 11:15:26 -0500

    primer hola mundo
    se hizo un hola mundo en consola

---
+++ Unity/Assets/Main/Codigos/Scripts/Variables.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("hola mundo la k el trap de medallo");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
